747e5e7577a387b6edbb271cdf9a2d02c518921c
hyeleenotharry <dlgpgl1029@gmail.com>
2025-03-09 04:38:58 +0900

    firebase refactoring

--- src/main/java/com/example/Daol_2025/config/FirebaseConfig.java
+++ src/main/java/com/example/Daol_2025/config/FirebaseConfig.java
@@ -6,9 +6,12 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.database.FirebaseDatabase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
@@ -16,7 +19,13 @@ public class FirebaseConfig {
     @Bean
     public FirebaseDatabase firebaseDatabase() throws IOException {
         // Load the Firebase service account key JSON file
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/daol-2025-firebase-adminsdk-fbsvc-dafc8a5b6b.json");
+        ClassPathResource resource = new ClassPathResource("daol-2025-firebase-adminsdk-fbsvc-dafc8a5b6b.json");
+
+        if (!resource.exists()) {
+            throw new FileNotFoundException("Firebase config file not found in classpath: " + resource.getPath());
+        }
+
+        InputStream serviceAccount = resource.getInputStream();
 
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
